d631e198c879c0b6664d3908c9dc1ab473930088
emmiejw <emilyprice84@outlook.com>
2018-11-13 13:57:13 +0000

    commented out notification until mailgun dns configured

--- app/Http/Controllers/TestimonialController.php
+++ app/Http/Controllers/TestimonialController.php
@@ -50,7 +50,7 @@ class TestimonialController extends Controller
     {
         $user = User::all();
         Testimonial::create($request->all());
-        Notification::send($user, new \App\Notifications\NewTestimonialPosted);
+        // Notification::send($user, new \App\Notifications\NewTestimonialPosted);
         // Session::flash('created', 'Post created succesfully.');
         return redirect()->action('ThankyouController@thankyou');
 
