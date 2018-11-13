e5c69860153cad8c95c22e2ef671704c74550a9b
emmiejw <emilyprice84@outlook.com>
2018-11-13 09:48:35 +0000

    css changed on footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,7 +3,7 @@
        <div class="jumbotron-fluid">
             <h3>Created by Emily Wallace - Price </h3>
             <h4><i class="fas fa-globe fa-spin"></i></i><b>  www.emilywallace-price.co.uk</b></h4>
-            <h4><i class="glyphicon glyphicon-envelope"></i><b>  emilywallaceprice@gmail.com</b></h4>
+            <h4><i class="glyphicon glyphicon-envelope"></i><b> <a href="mailto:emilywallaceprice@gmail.com">emilywallaceprice@gmail.com</a></b></h4>
        </div>
     </div>
 </div>
