f8e52501bd372e71a80df81503a483a7ffba91df
emmiejw <emilyprice84@outlook.com>
2018-11-20 23:31:24 +0000

    made contact page more dynamic

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -18,10 +18,10 @@
                               SO45 2QG <br>
                               <br>
                             <b>Call us on</b>:
-                              02380 897025 <br>
+                             <a href="tel:+02380 897025">02380 897025</a>  <br>
                               <br>
                             <b>Email us</b>: 
-                              steppingstones.hardley@gmail.com <br>
+                             <a href="mailto:steppingstones.hardley@gmail.com">steppingstones.hardley@gmail.com</a>  <br>
                               <br>
                               <br>
                                 <b>Ofsted URN - EY297420</b> <br>
@@ -31,7 +31,7 @@
                 </div>
                 <div class="col-sm-8">
                   <center>
-                    <img src="images/map.png" style="width: 40vmax;">
+                      <iframe class="googlemap" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10079.493700139514!2d-1.3909855!3d50.8335084!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xfae8ddc6af74397d!2sStepping+Stones+Pre-School!5e0!3m2!1sen!2suk!4v1542755678195" width="600" height="440" frameborder="0" style="border:0" allowfullscreen></iframe>
                   </center>  
                 </div> 
               
