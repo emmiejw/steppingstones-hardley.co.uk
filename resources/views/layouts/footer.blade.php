fbcbc1511865fe22b87ef62d39f7758357cbd531
emmiejw <emilyprice84@outlook.com>
2019-02-24 15:50:39 +0000

    footer updated with my web address

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="jumbotron jumbotron2">
        <div class="jumbotron-fluid">
             <h3>Created by Emily Wallace - Price </h3>
-            <h4><i class="fas fa-globe fa-spin"></i></i><b>  www.emilywallace-price.co.uk</b></h4>
+            <h4><i class="fas fa-globe fa-spin"></i></i><b><a href="https://emilywallace-price.co.uk/">emilywallace-price.co.uk</a></b></h4>
             <h4><i class="glyphicon glyphicon-envelope"></i><b> <a href="mailto:emilywallaceprice@gmail.com">emilywallaceprice@gmail.com</a></b></h4>
        </div>
     </div>
